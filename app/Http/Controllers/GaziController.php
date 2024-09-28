19dfc3ffc243976012da35dd4973b2825fd58c67
Gazi Akter <gaziakterdev@gmail.com>
2024-09-28 07:57:41 +0600

    update

---
+++ app/Http/Controllers/GaziController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GaziController extends Controller
+{
+    //
+    public function deffault(){
+        echo "Hello Bangladesh!";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GaziController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/gazi', [GaziController::class, 'deffault']);
