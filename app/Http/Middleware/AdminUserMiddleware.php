6b64cddc9592e88251975b6da1208a192bb57942
Gazi Akter <gaziakterdev@gmail.com>
2024-09-30 06:24:59 +0600

    use middleware

--- app/Http/Middleware/AdminUserMiddleware.php
+++ app/Http/Middleware/AdminUserMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Auth;
 
 class AdminUserMiddleware
 {
@@ -15,6 +16,12 @@ class AdminUserMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if(!empty(Auth::check())){
+            return $next($request);
+        } else{
+            Auth::logout();
+            return redirect(url(''));
+        }
+
     }
 }
