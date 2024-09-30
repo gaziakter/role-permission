f4324a44ba5b583d3c4515ff182d6ce53ac5fa06
Gazi Akter <gaziakterdev@gmail.com>
2024-09-30 07:05:58 +0600

    create sidebar route

--- resources/views/panel/layouts/sidebar.blade.php
+++ resources/views/panel/layouts/sidebar.blade.php
@@ -4,21 +4,21 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link " href="{{'panel/dashboard'}}">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile.html">
+        <a class="nav-link collapsed" href="{{'panel/user'}}">
           <i class="bi bi-person"></i>
           <span>User</span>
         </a>
       </li><!-- End Profile Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile.html">
+        <a class="nav-link collapsed" href="{{'panel/role'}}">
           <i class="bi bi-person"></i>
           <span>Role</span>
         </a>
