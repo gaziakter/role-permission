d4a3560355e4f1224d6489054c5d92a061746f09
Gazi Akter <gaziakterdev@gmail.com>
2024-10-01 19:37:22 +0600

    insert data

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\RoleModel;
 
 class RoleController extends Controller
 {
@@ -17,6 +18,10 @@ class RoleController extends Controller
         }
 
         public function insert(Request $request){
-            dd($request->all());
+            $save = new RoleModel;
+            $save->name = $request->name;
+            $save->save();
+
+            return redirect('panel/role')->with('succss', 'Role successfully created');
         }
 }
